71fff1899b8389f17a69c62d632037e3062def26
Antonio Fuller <mediainformationofficer@gmail.com>
2024-01-17 07:56:52 -0800

    implement fruit selection logic, display in Toast and log

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -10,5 +10,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // implement fruit selection logic, display in Toast and log to console
+        public static final String TAG= "MainActvity";
+        CheckBox apple,banana,orange,pineapple;
+        Button btnSelected;
+
+
+        apple = findViewById(R.id.apple);
+        banana = findViewById(R.id.banana);
+        orange = findViewById(R.id.orange);
+        pineapple = findViewById(R.id.pineapple);
+        btnSelected = findViewById(R.id.btnSelected);
+
+        btnSelected.setOnClickListener(view -> {
+            StringBuilder selectedFruits = new StringBuilder();
+            selectedFruits.append("Selected Items");
+
+            if (apple.isChecked()){
+                selectedFruits.append("\n Apple");
+            }
+            if (banana.isChecked()){
+                selectedFruits.append("\n Banana");
+            }
+            if (orange.isChecked()){
+                selectedFruits.append("\n Orange");
+            }
+            if (pineapple.isChecked()){
+                selectedFruits.append("\n Pineapple");
+            }
+
+            Toast.makeText(this, selectedFruits.toString(), Toast.LENGTH_SHORT).show();
+            Log.d(TAG, selectedFruits.toString());
+        }); // end btnSelected click event
+
     }
 }
